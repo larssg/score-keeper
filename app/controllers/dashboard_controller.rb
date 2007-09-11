76b1ffb5c1c1969bdd870535e52c1719836889d0
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-09-11 10:32:10 +0000

    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@41 f5341608-8839-0410-823a-0bb850aa70eb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @people = Person.find(:all, :order => 'games_won DESC').sort_by(&:winning_percentage).reverse
+    @game_count = Game.count
   end
 end
