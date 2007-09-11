498a4e27efeb63616335476a8753604013f3140e
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-09-11 09:09:48 +0000

    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@38 f5341608-8839-0410-823a-0bb850aa70eb

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -3,21 +3,21 @@ class UserNotifier < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://10.0.0.13:3000/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://10.0.0.13:3000/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "scorekeeper@larssehested.com"
+      @subject     = "[ScoreKeeper] "
       @sent_on     = Time.now
       @body[:user] = user
     end
