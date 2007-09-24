23a5cdfb64e99dac9b5dc2feeb9d8a19f0d9e8cd
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-09-24 22:12:56 +0000

    Protect from forgery
    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@104 f5341608-8839-0410-823a-0bb850aa70eb

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
 
   helper :all # include all helpers, all the time
+  protect_from_forgery
 
   def language
     session[:language]
