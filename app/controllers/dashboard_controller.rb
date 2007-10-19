c761fac8c06b8a07a81699db827c198cfb6683af
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-10-19 10:21:36 +0000

    Sweep game count cache.
    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@143 f5341608-8839-0410-823a-0bb850aa70eb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   def index
     unless read_fragment(dashboard_path)
       @people = Person.find(:all, :order => 'ranking DESC, games_won DESC, last_name')
+    end
+    unless read_fragment(dashboard_path + '_sidebar')
       @game_count = Game.count
     end
   end
