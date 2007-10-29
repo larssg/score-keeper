76abcae6522bf7b8becf5b21bf8956ce82b98c86
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-10-29 11:07:55 +0000

    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@188 f5341608-8839-0410-823a-0bb850aa70eb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    unless read_fragment(dashboard_path) && read_fragment(dashboard_path + '_sidebar')
+    unless cached?
       @people = Person.find(:all, :order => 'ranking DESC, games_won DESC, last_name')
       @recent_games = Game.find_recent(:limit => 8)
       @games_per_day = Game.count(:group => :played_on, :limit => 10, :order => 'played_on DESC')
@@ -10,5 +10,10 @@ class DashboardController < ApplicationController
       @game_count = Game.count
       @goals_scored = (Person.sum(:goals_for) + Person.sum(:goals_against)) / 4
     end
+  end
+  
+  protected
+  def cached?
+    read_fragment(dashboard_path) && read_fragment(dashboard_path + '_sidebar')
   end
 end
