8f38005364739e355da1ac590392d1ea0aec2468
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-11-08 14:15:58 +0000

    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@234 f5341608-8839-0410-823a-0bb850aa70eb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,14 @@ class DashboardController < ApplicationController
       @rankings = Person.find(:all, :order => 'ranking DESC, games_won DESC, last_name', :conditions => 'memberships_count >= 20')
       @newbies = Person.find(:all, :order => 'memberships_count DESC, ranking DESC, games_won DESC, last_name', :conditions => 'memberships_count < 20')
       @recent_games = Game.find_recent(:limit => 8)
-      @games_per_day = Game.count(:group => :played_on, :limit => 10, :order => 'played_on DESC')
+      @games_per_day = Game.count(:group => :played_on, :limit => 10, :order => 'games.played_on DESC')
 
       # Sidebar
       @leader = @rankings.size > 0 ? @rankings[0] : @newbies[0]
       @game_count = Game.count
       @goals_scored = Person.sum(:goals_for) / 2
+      @all_time_high = Membership.find(:first, :order => 'memberships.current_ranking DESC')
+      @all_time_low = Membership.find(:first, :order => 'memberships.current_ranking')
     end
 
     # Add game
