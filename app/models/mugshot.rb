23e4e38d6d25e90020254a6ec02469278ae70809
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-11-15 23:59:47 +0000

    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@246 f5341608-8839-0410-823a-0bb850aa70eb

--- app/models/mugshot.rb
+++ app/models/mugshot.rb
@@ -2,7 +2,7 @@ class Mugshot < ActiveRecord::Base
   has_attachment :content_type => :image, 
                  :storage => :file_system, 
                  :max_size => 2.megabytes,
-                 :thumbnails => { :thumb => '60x60>' }
+                 :thumbnails => { :thumb => '60x60>', :portrait => '220x300>' }
 
   validates_as_attachment
 end
