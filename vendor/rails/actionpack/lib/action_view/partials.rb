a5850616c04dcf493fbf24e842161855cb024e67
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-11-15 21:26:34 +0000

    Piston up
    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@241 f5341608-8839-0410-823a-0bb850aa70eb

--- vendor/rails/actionpack/lib/action_view/partials.rb
+++ vendor/rails/actionpack/lib/action_view/partials.rb
@@ -112,14 +112,14 @@ module ActionView
           add_counter_to_local_assigns!(partial_name, local_assigns)
           add_object_to_local_assigns!(partial_name, local_assigns, object)
 
-          if logger
+          if logger && logger.debug?
             ActionController::Base.benchmark("Rendered #{path}/_#{partial_name}", Logger::DEBUG, false) do
               render("#{path}/_#{partial_name}", local_assigns)
             end
           else
             render("#{path}/_#{partial_name}", local_assigns)
           end
-        when Array, ActiveRecord::Associations::AssociationCollection
+        when Array, ActiveRecord::Associations::AssociationCollection, ActiveRecord::Associations::HasManyThroughAssociation
           if partial_path.any?
             path       = ActionController::RecordIdentifier.partial_path(partial_path.first)
             collection = partial_path
