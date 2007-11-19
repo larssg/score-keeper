71fa53733e48ca7561c583ba8a42123000202f33
larssg <larssg@f5341608-8839-0410-823a-0bb850aa70eb>
2007-11-19 07:20:08 +0000

    git-svn-id: http://scorekeeper.googlecode.com/svn/trunk@264 f5341608-8839-0410-823a-0bb850aa70eb

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// Login form
 	$('#login_link').click(function() {
 		$(this).fadeOut('fast', function() {
-			$('#login_form').fadeIn('normal', function() {
+			$('#login_form').fadeIn('fast', function() {
 				$('#login_form .login').focus();
 			});
 		});
