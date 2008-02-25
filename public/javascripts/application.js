1be99a4bb2e0aa56208c16937f2b4810776049ac
Lars Sehested <lars@larssehested.com>
2008-02-25 11:47:38 +0100

    Removed login form in application layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-	// Login form
-	$('#login_link').click(function() {
-		$(this).fadeOut('fast', function() {
-			$('#login_form').fadeIn('fast', function() {
-				$('#login_form .login').focus();
-			});
-		});
-		return false;
-	});
-	
 	$('body.login #login').focus();
 });
