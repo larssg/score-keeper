792643110011f7c5353ab54c6da8cc5b500e2423
Lars Sehested <lars@larssehested.com>
2008-03-01 16:04:13 +0100

    Do not allow users to register accounts with reserved words as domain names

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,6 +6,7 @@ class Account < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :domain
   validates_format_of :domain, :with => /^[0-9a-z]+$/, :on => :create, :message => "can only contain alphanumeric lowercase characters"
+  validates_exclusion_of :domain, :in => %w(www pop pop3 imap smtp mail support ftp mysql), :on => :create, :message => "is not allowed"
 
   def all_users
     @all_users ||= self.users.find(:all, :order => 'name, display_name')
