36055ce830519a62941e3fed06d49b517e524ccf
Lars Sehested <lars@larssehested.com>
2008-03-04 08:49:41 +0100

    Remove tests for user signup as signup has been disabled

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,51 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe UsersController do
-  fixtures :users
-
-  it 'allows signup' do
-    lambda do
-      create_user
-      response.should be_redirect      
-    end.should change(User, :count).by(1)
-  end
-
-  it 'requires login on signup' do
-    lambda do
-      create_user(:login => nil)
-      assigns[:user].errors.on(:login).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
+  fixtures :users, :accounts
   
-  it 'requires password on signup' do
-    lambda do
-      create_user(:password => nil)
-      assigns[:user].errors.on(:password).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
+  before(:each) do
+    controller.stub!(:current_account).and_return(accounts(:champions))
   end
-  
-  it 'requires password confirmation on signup' do
-    lambda do
-      create_user(:password_confirmation => nil)
-      assigns[:user].errors.on(:password_confirmation).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-
-  it 'requires email on signup' do
-    lambda do
-      create_user(:email => nil)
-      assigns[:user].errors.on(:email).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  
   
   def create_user(options = {})
     post :create, :user => { :login => 'quire', :email => 'quire@example.com',
-      :password => 'quire', :password_confirmation => 'quire' }.merge(options)
+      :password => 'quire', :password_confirmation => 'quire',
+      :name => 'Quire', :display_name => 'Mr. Quire' }.merge(options)
   end
 end
