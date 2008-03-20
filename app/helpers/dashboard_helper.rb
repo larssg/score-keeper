4c5dd7a68574d91ba1c4831b95691a418827704d
Lars Sehested <larssg@gmail.com>
2008-03-20 23:40:46 +0100

    Show matches played graph in chronological order

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,7 +2,7 @@ module DashboardHelper
   def matches_per_day_chart(matches_per_day)
     require 'google_chart'
     GoogleChart::LineChart.new('230x120') do |bc|
-      bc.data "Matches per day"[], matches_per_day.collect { |day| day[1] }, '77BBDD'
+      bc.data "Matches per day"[], matches_per_day.reverse.collect { |day| day[1] }, '77BBDD'
       bc.show_legend = false
       return bc.to_url
     end
