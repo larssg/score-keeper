6223a2229a59e94850cc6e30ac077edb540ca84d
Lars Sehested <larssg@gmail.com>
2008-03-20 23:57:31 +0100

    Add y-axis to matches-per-day chart

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,10 +1,12 @@
 module DashboardHelper
   def matches_per_day_chart(matches_per_day)
     require 'google_chart'
-    GoogleChart::LineChart.new('230x120') do |bc|
-      bc.data "Matches per day"[], matches_per_day.reverse.collect { |day| day[1] }, '77BBDD'
-      bc.show_legend = false
-      return bc.to_url
+    data = matches_per_day.reverse.collect { |day| day[1].to_i }
+    GoogleChart::LineChart.new('230x120') do |lc|
+      lc.data 'Matches per day'[], data, '77BBDD'
+      lc.axis :y, :range => [0, data.max], :color => '000000', :font_size => 12, :alignment => :center
+      lc.show_legend = false
+      return lc.to_url
     end
   end
 end
