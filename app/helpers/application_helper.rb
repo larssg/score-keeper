eeb35d4056adda8eba14aeaa967b64603cc3213e
Lars Sehested <larssg@gmail.com>
2008-04-05 21:47:29 +0200

    Improved form layout for account settings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def title(page_title)
+  def title(page_title, options = {})
     @page_title = page_title
+    @page_title_options = options
   end
   
   def page_title
