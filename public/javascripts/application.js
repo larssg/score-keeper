c19daf8c32ae4faab34a467ce73622f82aa9561c
Lars Sehested <larssg@gmail.com>
2008-05-02 00:34:04 +0200

    Add sort to dashboard and team list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 	$('body.login #login').focus();
+
+	$('#rankings').tablesorter({
+		sortList: [[0, 0]],
+		widgets: ['zebra'],
+		headers: {
+			1: { sorter: false },
+			2: { sorter: false }
+		}
+	});
+	
+	$('#newbies').tablesorter({
+		sortList: [[0, 0]],
+		widgets: ['zebra'],
+		headers: {
+			1: { sorter: false }
+		}
+	});
+	
+	$('#teams').tablesorter({
+		sortList: [[1, 1]],
+		widgets: ['zebra'],
+		headers: {
+			0: { sorter: false },
+			5: { sorter: false }
+		}
+	});
 });
