13fb3e600d55d9a513849fc3e058f764065f4702
Lars Sehested <larssg@gmail.com>
2008-05-02 03:35:45 +0200

    Load js at the bottom, only load swfobject on graph pages

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,10 +1,20 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe GamesController do
+  fixtures :users, :accounts
 
-  #Delete this example and add some real ones
-  it "should use GamesController" do
-    controller.should be_an_instance_of(GamesController)
+  before(:each) do
+    @game = Factory.create_game(:account => accounts(:champions))
+    controller.stub!(:current_game).and_return(@game)
+    controller.stub!(:current_account).and_return(accounts(:champions))
+    login_as :aaron
   end
 
+  #Delete this example and add some real ones
+  it "should list the games" do
+    get :index
+    response.should be_success
+    assigns[:games].should_not be_nil
+    assigns[:games].size.should == 1
+  end
 end
