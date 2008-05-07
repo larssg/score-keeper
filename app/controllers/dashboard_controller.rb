e376e05ed013956b8e4ecd00a3fa719624e5820b
Lars Sehested <larssg@gmail.com>
2008-05-07 15:35:17 +0200

    Update dashboard automatically - needs auto-update of sidebar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,38 +4,46 @@ class DashboardController < ApplicationController
 
   def index
     @logs = current_game.logs.find(:all, :order => 'published_at DESC', :limit => 5)
-    @recent_matches = current_game.matches.find_recent(nil, :limit => 10, :include => { :teams => :memberships })
-
-    unless cached?
-      @rankings = current_game.ranked_game_participators
-      @newbies = current_game.newbie_game_participators
-      @matches_per_day = current_game.matches.count(:group => :played_on, :limit => 10, :order => 'matches.played_on DESC')
-
-      # Sidebar
-      @leader = @rankings.size > 0 ? @rankings[0] : @newbies[0]
-      @match_count = current_game.matches.count
-      
-      total_points_for = current_game.game_participations.sum(:points_for)
-      @points = total_points_for.nil? ? 0 : total_points_for / current_game.team_size
-
-      @all_time_high = Membership.all_time_high(current_game)
-      @all_time_low = Membership.all_time_low(current_game)
-
-      # Get positions 7 days ago
-      @positions = {}
-      last_month = current_game.matches.find(:first, :order => 'played_at ASC', :conditions => ['played_at >= ?', 7.days.ago])
-      unless last_month.blank? || last_month.positions.blank?
-        last_month.positions.each_with_index do |user_id, index|
-          @positions[user_id] = {}
-          @positions[user_id][:now] = find_user(user_id).position(current_game)
-          @positions[user_id][:then] = index + 1
+    respond_to do |format|
+      format.js do
+        if @logs.first.published_at.to_s(:db) != params[:last_update]
+          load_data
+          render :action => 'index', :layout => false
+        else
+          head 200
         end
       end
+      format.html { load_data }
     end
   end
-
+  
   protected
-  def cached?
-    read_fragment(cache_key) && read_fragment(cache_key('sidebar'))
+  def load_data
+    @recent_matches = current_game.matches.find_recent(nil, :limit => 10, :include => { :teams => :memberships })
+
+    @rankings = current_game.ranked_game_participators
+    @newbies = current_game.newbie_game_participators
+    @matches_per_day = current_game.matches.count(:group => :played_on, :limit => 10, :order => 'matches.played_on DESC')
+
+    # Sidebar
+    @leader = @rankings.size > 0 ? @rankings[0] : @newbies[0]
+    @match_count = current_game.matches.count
+    
+    total_points_for = current_game.game_participations.sum(:points_for)
+    @points = total_points_for.nil? ? 0 : total_points_for / current_game.team_size
+
+    @all_time_high = Membership.all_time_high(current_game)
+    @all_time_low = Membership.all_time_low(current_game)
+
+    # Get positions 7 days ago
+    @positions = {}
+    last_month = current_game.matches.find(:first, :order => 'played_at ASC', :conditions => ['played_at >= ?', 7.days.ago])
+    unless last_month.blank? || last_month.positions.blank?
+      last_month.positions.each_with_index do |user_id, index|
+        @positions[user_id] = {}
+        @positions[user_id][:now] = find_user(user_id).position(current_game)
+        @positions[user_id][:then] = index + 1
+      end
+    end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,39 +1,91 @@
+var scoreKeeper = {
+	matchUpdate: {
+		interval: 60,
+		
+		init: function() {
+			if (scoreKeeper.matchUpdate.isShowingDashboard()) {
+				scoreKeeper.matchUpdate.run();
+			}
+		},
+		
+		run: function() {
+			$.get('/', {
+					game_id: scoreKeeper.matchUpdate.gameId(),
+					last_update: scoreKeeper.matchUpdate.lastUpdate()
+				},
+				function(data, status) {
+					setTimeout(scoreKeeper.matchUpdate.run, scoreKeeper.matchUpdate.interval * 1000);
+					if (jQuery.trim(data) != '') {
+						$('#main').html(data);
+						scoreKeeper.tableSort();
+					}
+				}
+			);
+		},
+		
+		isShowingDashboard: function() {
+			return $('#logs').length > 0;
+		},
+		
+		lastUpdate: function() {
+			return scoreKeeper.matchUpdate.getValue('last_update');
+		},
+		
+		gameId: function() {
+			return scoreKeeper.matchUpdate.getValue('game_id');
+		},
+		
+		getValue: function(value) {
+			var elements = $("#dashboard_data ." + value);
+			if (elements.length > 0) {
+				var element = elements[0];
+				return element.innerHTML;
+			}
+			return false;
+		}
+	},
+	
+	tableSort: function() {
+		$('#rankings').tablesorter({
+			sortList: [[0, 0]],
+			widgets: ['zebra'],
+			headers: {
+				1: { sorter: false },
+				2: { sorter: false }
+			}
+		});
+
+		$('#newbies').tablesorter({
+			sortList: [[0, 0]],
+			widgets: ['zebra'],
+			headers: {
+				1: { sorter: false }
+			}
+		});
+
+		$('#teams').tablesorter({
+			sortList: [[1, 1]],
+			widgets: ['zebra'],
+			headers: {
+				0: { sorter: false },
+				5: { sorter: false }
+			}
+		});
+
+		$('#teammates').tablesorter({
+			sortList: [[6, 1]],
+			widgets: ['zebra'],
+			headers: {
+				0: { sorter: false },
+				1: { sorter: false },
+				2: { sorter: false }
+			}
+		});
+	},
+};
+
 $(document).ready(function() {
 	$('body.login #login').focus();
-
-	$('#rankings').tablesorter({
-		sortList: [[0, 0]],
-		widgets: ['zebra'],
-		headers: {
-			1: { sorter: false },
-			2: { sorter: false }
-		}
-	});
-	
-	$('#newbies').tablesorter({
-		sortList: [[0, 0]],
-		widgets: ['zebra'],
-		headers: {
-			1: { sorter: false }
-		}
-	});
-	
-	$('#teams').tablesorter({
-		sortList: [[1, 1]],
-		widgets: ['zebra'],
-		headers: {
-			0: { sorter: false },
-			5: { sorter: false }
-		}
-	});
-	
-	$('#teammates').tablesorter({
-		sortList: [[6, 1]],
-		widgets: ['zebra'],
-		headers: {
-			0: { sorter: false },
-			1: { sorter: false },
-			2: { sorter: false }
-		}
-	});
+	scoreKeeper.tableSort();
+	scoreKeeper.matchUpdate.init();
 });
