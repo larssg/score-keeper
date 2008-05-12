ac8af665926382be6bdd88947f4d5f97e2e3e827
Lars Sehested <larssg@gmail.com>
2008-05-12 19:31:21 +0200

    (Hopefully) improve fove front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
   
   layout 'public'
   
+  def index
+    @news = NewsItem.find(:all, :order => 'posted_at DESC', :limit => 2)
+  end
+  
   def login
     redirect_to account_url(params[:domain])
   end
