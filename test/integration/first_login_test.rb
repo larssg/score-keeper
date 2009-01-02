ed4d321f2bb5c07e19e3e6100d2fd618aaa785fa
Lars G. Sehested <larssg@birgitte-desktop.(none)>
2009-01-02 04:01:19 +0100

    Made /games/new work when there are no games in the database

--- test/integration/first_login_test.rb
+++ test/integration/first_login_test.rb
@@ -2,11 +2,8 @@ require 'test_helper'
 
 class FirstLoginTest < ActionController::IntegrationTest
   test "should be able to go to the dashboard on first login" do
-    @user = Factory(:user)
-    
-    post "http://#{@user.account.domain}.example.com/sessions", :login => @user.login, :password => @user.password
-    assert_response :redirect
-    assert_redirected_to '/'
+    user = Factory(:user)
+    login(user)
     
     get '/'
     assert_response :redirect
@@ -14,5 +11,19 @@ class FirstLoginTest < ActionController::IntegrationTest
 
     get '/games'
     assert_response :success
+  end
+
+  test "should show /games/new" do
+    user = Factory(:user)
+    login(user)
+
+    get '/games/new'
+    assert_response :success
+  end
+
+  def login(user)
+    post "http://#{user.account.domain}.example.com/sessions", :login => user.login, :password => user.password
+    assert_response :redirect
+    assert_redirected_to '/'
   end
 end
