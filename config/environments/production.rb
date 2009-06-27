e8209a17a8f9059e5d699c5759a45f3423bf8f64
Lars G. Sehested <lars@larssehested.com>
2009-06-27 18:53:51 +0200

    Perform caching in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 config.action_controller.asset_host                  = "http://assets%d.scorekeepr.dk"
