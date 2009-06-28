f988b2878c0c1f65a5543d3ee654a43880a53cd1
Lars G. Sehested <lars@larssehested.com>
2009-06-28 06:44:02 +0200

    Always assume html when redirecting for login - fixes IE bug

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -61,15 +61,8 @@ module AuthenticatedSystem
     # to access the requested action.  For example, a popup window might
     # simply close itself.
     def access_denied
-      respond_to do |format|
-        format.html do
-          store_location
-          redirect_to login_path
-        end
-        format.any do
-          request_http_basic_authentication 'Web Password'
-        end
-      end
+      store_location
+      redirect_to login_path
     end
 
     # Store the URI of the current request in the session.
