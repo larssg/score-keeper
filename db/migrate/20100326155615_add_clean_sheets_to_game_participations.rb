c2f7353263a3622d40ff6e882c9f0b3db60ffdff
Lars G. Sehested <lars@larssehested.com>
2010-03-26 19:13:36 +0100

    My head must not be working...

--- db/migrate/20100326155615_add_clean_sheets_to_game_participations.rb
+++ db/migrate/20100326155615_add_clean_sheets_to_game_participations.rb
@@ -2,11 +2,9 @@ class AddCleanSheetsToGameParticipations < ActiveRecord::Migration
   def self.up
     add_column :game_participations, :clean_sheets, :integer, :default => 0
     
-    Team.find_each do |team|
-      if team.score == 0
-        team.other.memberships.each do |membership|
-          membership.game_participation.increment!(:clean_sheets)
-        end
+    Team.find_each(:conditions => 'score = 0') do |team|
+      team.other.memberships.each do |membership|
+        membership.game_participation.increment!(:clean_sheets)
       end
     end
   end
