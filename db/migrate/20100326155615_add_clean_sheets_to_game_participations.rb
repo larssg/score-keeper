ca7f9e65544d097c5ba9e95076b1238bd3e51fa6
Lars G. Sehested <lars@larssehested.com>
2010-03-26 17:24:43 +0100

    Update clean sheets in batches of 1000 teams and improve performance.

--- db/migrate/20100326155615_add_clean_sheets_to_game_participations.rb
+++ db/migrate/20100326155615_add_clean_sheets_to_game_participations.rb
@@ -2,9 +2,9 @@ class AddCleanSheetsToGameParticipations < ActiveRecord::Migration
   def self.up
     add_column :game_participations, :clean_sheets, :integer, :default => 0
     
-    Team.all.each do |team|
-      if team.other.score == 0
-        team.memberships.each do |membership|
+    Team.find_each do |team|
+      if team.score == 0
+        team.other.memberships.each do |membership|
           membership.game_participation.increment!(:clean_sheets)
         end
       end
