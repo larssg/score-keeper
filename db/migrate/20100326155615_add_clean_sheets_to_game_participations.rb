bdd0725ba026b4ad72d72b083e604afc5f6fc612
Lars G. Sehested <lars@larssehested.com>
2010-03-26 22:57:28 +0100

    Dumbify migration (head getting foggier and foggier...)

--- db/migrate/20100326155615_add_clean_sheets_to_game_participations.rb
+++ db/migrate/20100326155615_add_clean_sheets_to_game_participations.rb
@@ -2,10 +2,9 @@ class AddCleanSheetsToGameParticipations < ActiveRecord::Migration
   def self.up
     add_column :game_participations, :clean_sheets, :integer, :default => 0
     
-    Team.find_each(:conditions => 'score = 0') do |team|
-      other_team = team.other
-      unless other_team.nil?
-        other_team.memberships.each do |membership|
+    Team.find_each do |team|
+      if team.score == 0
+        team.other.memberships.each do |membership|
           membership.game_participation.increment!(:clean_sheets)
         end
       end
