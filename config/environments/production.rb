f346705282806176f3e108075b4c629a1dc8c13d
Lars Geisler Sehested <lars@larssehested.com>
2010-05-27 18:39:00 +0200

    Switch to 3 asset hosts.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-config.action_controller.asset_host                  = "http://assets%d.scorekeepr.dk"
+config.action_controller.asset_host                  = Proc.new { |source| "http://assets#{source.hash % 3}.scorekeepr.dk" }
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
