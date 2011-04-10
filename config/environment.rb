85a04d8f9633bb96f91b5e2a976bd86b1a4f3426
Lars Geisler Sehested <lars@larssehested.com>
2011-04-10 20:38:39 +0200

    Remove Dalli clear code.

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ScoreKeeper::Application.initialize!
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    # Only works with DalliStore
-    Rails.cache.reset if forked
-  end
-end
