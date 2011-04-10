2540a31bb32fdd14ebcc4a5c1408a51580f1f80b
Lars Geisler Sehested <lars@larssehested.com>
2011-04-10 21:07:23 +0200

    Remove jquery.fx and tablesorter.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,21 +3,6 @@ var scoreKeeper = {
         toggle: function() {
             var element = $('#add_match_form');
             element.toggle();
-            if (element.css('display') == 'block') {
-                scoreKeeper.matchUpdate.interval = 10;
-                $.get('/games/' + scoreKeeper.matchUpdate.gameId() + '/game_added_warning',
-                      {},
-                      function(data, status) {
-                          if (status == 'success') {
-                              $('#add_match_form .game_added_warning').html(data);
-                              $('#add_match_form .game_added_warning').show();
-                          }
-                      });
-            } else {
-                scoreKeeper.matchUpdate.interval = 60;
-                $('#add_match_form .game_added_warning').html('');
-                $('#add_match_form .game_added_warning').hide();
-            }
         }
     },
 
@@ -60,53 +45,18 @@ var scoreKeeper = {
         gameId: function() {
             return $('#dashboard_data .game_id').html();
         }
-    },
-
-    tableSort: function() {
-        $('#rankings').tablesorter({
-            sortList: [[1, 0]],
-            widgets: ['zebra'],
-            headers: {
-                0: { sorter: false },
-                2: { sorter: false },
-                3: { sorter: false }
-            }
-        });
-
-        $('#newbies').tablesorter({
-            sortList: [[1, 0]],
-            widgets: ['zebra'],
-            headers: {
-                0: { sorter: false },
-                2: { sorter: false }
-            }
-        });
-
-        $('#teams').tablesorter({
-            sortList: [[1, 1]],
-            widgets: ['zebra'],
-            headers: {
-                0: { sorter: false },
-                5: { sorter: false }
-            }
-        });
-
-        $('#teammates').tablesorter({
-            sortList: [[6, 1]],
-            widgets: ['zebra'],
-            headers: {
-                0: { sorter: false },
-                1: { sorter: false },
-                2: { sorter: false }
-            }
-        });
     }
 };
 
 $(document).ready(function() {
     $('body.login #login').focus();
-    scoreKeeper.tableSort();
     scoreKeeper.matchUpdate.init();
+
+    $('#add_match_button').click(function() {
+      scoreKeeper.newMatch.toggle();
+      return false;
+    });
+
     if ($('#messages').length > 0) {
         setTimeout(function() { $('#messages').fadeOut('slow'); }, 8000);
     }
