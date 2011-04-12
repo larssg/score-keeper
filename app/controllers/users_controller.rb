f80b51574c193a405f8c24ccb94d930a913a84ba
Lars Geisler Sehested <lars@larssehested.com>
2011-04-12 23:56:34 +0200

    Dont load team data for games with a team size of 1.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,30 +21,32 @@ class UsersController < ApplicationController
     @all_time_low = @user.all_time_low(@game)
     @matches_per_day = @game_participation.memberships.count(:group => 'matches.played_on', :limit => 10, :order => 'matches.played_on DESC', :joins => 'LEFT JOIN teams ON memberships.team_id = teams.id LEFT JOIN matches ON teams.match_id = matches.id')
 
-    @team_counts = @user.teams.count(:group => :team_ids, :conditions => ['memberships.game_id = ?', @game.id]).sort_by { |t| t[1] }.reverse
-    @team_wins = @user.teams.joins(:memberships).where(:won => true, :memberships => { :game_id => @game.id }).group('team_ids').count
-    @teams = current_account.teams.find(:all, :group => :team_ids, :conditions => { :team_ids => @team_counts.collect { |tc| tc[0] } })
+    if @game.team_size > 1
+      @team_counts = @user.teams.count(:group => :team_ids, :conditions => ['memberships.game_id = ?', @game.id]).sort_by { |t| t[1] }.reverse
+      @team_wins = @user.teams.joins(:memberships).where(:won => true, :memberships => { :game_id => @game.id }).group('team_ids').count
+      @teams = current_account.teams.find(:all, :group => :team_ids, :conditions => { :team_ids => @team_counts.collect { |tc| tc[0] } })
 
-    @teams = @teams.collect do |team|
-      {
-        :team => team,
-        :played => @team_counts.select { |tc| tc[0] == team.team_ids }[0],
-        :wins => @team_wins[team.team_ids].to_i / 2
-      }
-    end
-    
-    if @game.team_size == 2
-      @teams.each do |team|
-        team_mate = team[:team].team_mate_for(@user)
-        team[:team_mate] = team_mate
-        team[:team_mate_game_participation] = @game.game_participation_for(team_mate)
-        team[:played] = team[:played].blank? ? 0 : team[:played][1].blank? ? 0 : team[:played][1].to_i
-        team[:wins] = team[:wins].blank? ? 0 : team[:wins]
-        team[:win_percentage] = "%01.1f" % (team[:wins].to_f * 100.0 / team[:played].to_f)
+      @teams = @teams.collect do |team|
+        {
+          :team => team,
+          :played => @team_counts.select { |tc| tc[0] == team.team_ids }[0],
+          :wins => @team_wins[team.team_ids].to_i / 2
+        }
       end
-    end
+    
+      if @game.team_size == 2
+        @teams.each do |team|
+          team_mate = team[:team].team_mate_for(@user)
+          team[:team_mate] = team_mate
+          team[:team_mate_game_participation] = @game.game_participation_for(team_mate)
+          team[:played] = team[:played].blank? ? 0 : team[:played][1].blank? ? 0 : team[:played][1].to_i
+          team[:wins] = team[:wins].blank? ? 0 : team[:wins]
+          team[:win_percentage] = "%01.1f" % (team[:wins].to_f * 100.0 / team[:played].to_f)
+        end
+      end
 
-    @teams = @teams.sort_by { |t| t[:win_percentage].to_f }.reverse
+      @teams = @teams.sort_by { |t| t[:win_percentage].to_f }.reverse
+    end
     
     @json = chart_json(@time_period, @game_participation)
   rescue ActiveRecord::RecordNotFound
