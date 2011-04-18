b90c5e27622cf69b0de8d44b132f4e16bb41f76e
Lars Geisler Sehested <lars@larssehested.com>
2011-04-18 14:51:57 +0200

    Remove matches_per_day_chart helper.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,15 +1,4 @@
 module DashboardHelper
-  def matches_per_day_chart(matches_per_day)
-    require 'google_chart'
-    data = matches_per_day.reverse.collect { |day| day[1].to_i }
-    GoogleChart::LineChart.new('216x120') do |lc|
-      lc.data 'Matches per day', data, '77BBDD'
-      lc.axis :y, :range => [0, data.max], :color => '000000', :font_size => 12, :alignment => :center
-      lc.show_legend = false
-      return lc.to_url
-    end
-  end
-
   def position_icon(position)
     return '' if position.blank?
 
