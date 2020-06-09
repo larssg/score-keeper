f6698c23cc7d30e89eb7afa4f2e5f01152d59792
stickler-ci <support@stickler-ci.com>
2020-06-09 12:21:51 +0000

    Fixing style errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -75,12 +75,17 @@ class ApplicationController < ActionController::Base
   def current_game
     if @current_game.nil?
       return unless current_account
-      @current_game ||= current_account.games.first(:conditions => { :id => session[:current_game_id] }) if session[:current_game_id]
+
+      if session[:current_game_id]
+        @current_game ||= current_account.games.first(conditions: { id: session[:current_game_id] })
+      end
       @current_game ||= current_account.games.first
-      unless @current_game.nil?
-        session[:current_game_id] ||= @current_game.id
+      if @current_game.nil?
+        unless %w[games sessions users accounts].include?(controller_name)
+          redirect_to games_url
+        end
       else
-        redirect_to games_url unless %w[games sessions users accounts].include?(controller_name)
+        session[:current_game_id] ||= @current_game.id
       end
     end
     @current_game
