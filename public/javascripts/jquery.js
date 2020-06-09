7532947ca2e79d4fc8150ad3c50173f74bb9730f
Awesome Code <team@xinminlabs.com>
2020-06-09 12:26:58 +0000

    Auto corrected by following Lint Javascript unicorn/prefer-includes

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -794,7 +794,7 @@
           rwebkit.exec(ua) ||
           ropera.exec(ua) ||
           rmsie.exec(ua) ||
-          (ua.indexOf("compatible") < 0 && rmozilla.exec(ua)) ||
+          (!ua.includes("compatible") && rmozilla.exec(ua)) ||
           [];
 
         return { browser: match[1] || "", version: match[2] || "0" };
@@ -1761,7 +1761,7 @@
                 setClass = elem.className;
 
               for (var c = 0, cl = classNames.length; c < cl; c++) {
-                if (className.indexOf(" " + classNames[c] + " ") < 0) {
+                if (!className.includes(" " + classNames[c] + " ")) {
                   setClass += " " + classNames[c];
                 }
               }
@@ -1845,7 +1845,7 @@
     hasClass: function (selector) {
       var className = " " + selector + " ";
       for (var i = 0, l = this.length; i < l; i++) {
-        if ((" " + this[i].className + " ").replace(rclass, " ").indexOf(className) > -1) {
+        if ((" " + this[i].className + " ").replace(rclass, " ").includes(className)) {
           return true;
         }
       }
@@ -2179,7 +2179,7 @@
         handleObj = handleObjIn ? jQuery.extend({}, handleObjIn) : { handler: handler, data: data };
 
         // Namespaced event handlers
-        if (type.indexOf(".") > -1) {
+        if (type.includes(".")) {
           namespaces = type.split(".");
           type = namespaces.shift();
           handleObj.namespace = namespaces.slice(0).sort().join(".");
@@ -2289,7 +2289,7 @@
       while ((type = types[i++])) {
         origType = type;
         handleObj = null;
-        all = type.indexOf(".") < 0;
+        all = !type.includes(".");
         namespaces = [];
 
         if (!all) {
@@ -2388,7 +2388,7 @@
             : // Just the event type (string)
               jQuery.Event(type);
 
-        if (type.indexOf("!") >= 0) {
+        if (type.includes("!")) {
           event.type = type = type.slice(0, -1);
           event.exclusive = true;
         }
@@ -2506,7 +2506,7 @@
       event.currentTarget = this;
 
       // Namespaced event handlers
-      all = event.type.indexOf(".") < 0 && !event.exclusive;
+      all = !event.type.includes(".") && !event.exclusive;
 
       if (!all) {
         namespaces = event.type.split(".");
@@ -3818,7 +3818,7 @@
           for (var i = 0, elem; (elem = curLoop[i]) != null; i++) {
             if (elem) {
               if (
-                not ^ (elem.className && (" " + elem.className + " ").replace(/[\t\n\r]/g, " ").indexOf(match) >= 0)
+                not ^ (elem.className && (" " + elem.className + " ").replace(/[\t\n\r]/g, " ").includes(match))
               ) {
                 if (!inplace) {
                   result.push(elem);
@@ -4037,7 +4037,7 @@
           if (filter) {
             return filter(elem, i, match, array);
           } else if (name === "contains") {
-            return (elem.textContent || elem.innerText || Sizzle.getText([elem]) || "").indexOf(match[3]) >= 0;
+            return (elem.textContent || elem.innerText || Sizzle.getText([elem]) || "").includes(match[3]);
           } else if (name === "not") {
             var not = match[3];
 
@@ -4123,7 +4123,7 @@
         },
 
         CLASS: function (elem, match) {
-          return (" " + (elem.className || elem.getAttribute("class")) + " ").indexOf(match) > -1;
+          return (" " + (elem.className || elem.getAttribute("class")) + " ").includes(match);
         },
 
         ATTR: function (elem, match) {
@@ -4142,9 +4142,9 @@
             : type === "="
             ? value === check
             : type === "*="
-            ? value.indexOf(check) >= 0
+            ? value.includes(check)
             : type === "~="
-            ? (" " + value + " ").indexOf(check) >= 0
+            ? (" " + value + " ").includes(check)
             : !check
             ? value && result !== false
             : type === "!="
